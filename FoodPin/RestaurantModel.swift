1a5bfb64ea5fccb8d706cd124c4286ef96e95d22
Arman Jon Villalobos <me@armanjon.com>
2016-05-17 12:16:03 +0800

    create a new model to hold the information

---
+++ FoodPin/RestaurantModel.swift
@@ -0,0 +1,110 @@
+//
+//  RestaurantModel.swift
+//  FoodPin
+//
+//  Created by Arman Jon Villalobos on 17/05/2016.
+//  Copyright © 2016 Arman Jon Villalobos. All rights reserved.
+//
+
+import Foundation
+
+class RestaurantModel {
+
+    var names = [
+        "Cafe Deadend",
+        "Homei",
+        "Teakha",
+        "Cafe Loisl",
+        "Petite Oyster",
+        "For Kee Restaurant",
+        "Po's Atelier",
+        "Bourke Street Bakery",
+        "Haigh'sChocolate",
+        "Palomino Espresso",
+        "Upstate",
+        "Traif",
+        "Graham Avenue Meats",
+        "Waffle & Wolf",
+        "Five Leaves",
+        "Cafe Lore",
+        "Confessional",
+        "Barrafina",
+        "Donostia",
+        "Royal Oak",
+        "Thai Cafe"
+    ]
+    
+    var images = [
+        "cafedeadend.jpg",
+        "homei.jpg",
+        "teakha.jpg",
+        "cafeloisl.jpg",
+        "petiteoyster.jpg",
+        "forkeerestaurant.jpg",
+        "posatelier.jpg",
+        "bourkestreetbakery.jpg",
+        "haighschocolate.jpg",
+        "palominoespresso.jpg",
+        "upstate.jpg",
+        "traif.jpg",
+        "grahamavenuemeats.jpg",
+        "wafflewolf.jpg",
+        "fiveleaves.jpg",
+        "cafelore.jpg",
+        "confessional.jpg",
+        "barrafina.jpg",
+        "donostia.jpg",
+        "royaloak.jpg",
+        "thaicafe.jpg"
+    ]
+    
+    var locations = [
+        "Hong Kong",
+        "Hong Kong",
+        "Hong Kong",
+        "Hong Kong",
+        "Hong Kong",
+        "Hong Kong",
+        "Hong Kong",
+        "Sydney",
+        "Sydney",
+        "Sydney",
+        "New York",
+        "New York",
+        "New York",
+        "New York",
+        "New York",
+        "New York",
+        "New York",
+        "London",
+        "London",
+        "London",
+        "London"
+    ]
+    
+    var types = [
+        "Coffee & Tea Shop",
+        "Cafe",
+        "Tea House",
+        "Austrian / Causual Drink",
+        "French",
+        "Bakery",
+        "Bakery",
+        "Chocolate",
+        "Cafe",
+        "American / Seafood",
+        "American",
+        "American",
+        "Breakfast & Brunch",
+        "Coffee & Tea",
+        "Coffee & Tea",
+        "Latin American",
+        "Spanish",
+        "Spanish",
+        "Spanish",
+        "British",
+        "Thai"
+    ]
+
+
+}
